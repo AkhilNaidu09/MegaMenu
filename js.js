a24ac6e743a40163a13700823dc99a66b10734e2
Akhil <akil.naidu28@gmail.com>
2018-01-05 15:32:15 +0530

    Hide and show functionalities on click menu items

--- js.js
+++ js.js
@@ -8,9 +8,19 @@
 		},
 		bind: function () {
 			department.click(function (event) {
-
+				event.stopPropagation();
 				drop_down.slideToggle("fast");
 			});
+
+			$(document).click(function (event) {
+				if (!$(event.target).closest('.left_box').length) {
+
+					drop_down.hide();
+				} else {
+					event.stopPropagation();
+				}
+			});
+
 		},
 		slide: function () {
 
